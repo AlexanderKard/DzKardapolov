612da537b2fbebec6baf26c38e436950e940afcc
Alex <geni46@mail.ru>
2022-12-06 07:37:24 +0500

    osnova

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача №2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.Write("Imput a first number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Imput a second number: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+// int min = a;
+
+// if (a < b) max = b;
+// if (a > b) min = b;
+
+// Console.WriteLine("Max num " + max);
+// Console.WriteLine("Min num " + min); 
+
+
+// Здача №4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Console.Write("Imput a first number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Imput a second number: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Imput a second number: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+
+// Console.WriteLine("Max num " + max);
+
+
+// Задача №6Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Console.Write("Imput a number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a % 2 == 0) 
+// {
+//     Console.Write("Число четное");
+// }
+// else
+// {
+//     Console.Write("Число не четное");
+// }
+
+
+
+// Задача 8
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Честно пытался, но не получилось. Я понимаю нужно как в 6 задаче делить на 2 == 0, но как это в цикле реализовать
+// не понимаю
